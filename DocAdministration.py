b55accd91adadebab7c7b6b8c05f2e4f66a642a6
lukebrr <50903167+lukebrr@users.noreply.github.com>
2019-05-30 11:15:57 +0800

    Update DocAdministration.py

--- DocAdministration.py
+++ DocAdministration.py
@@ -1,1 +1,13 @@
 #Document Administration - All combined
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/api/v1/AddDoc',methods=['PUT'])
+def status():
+    response = {'status' : 'Putting up a Doc'}
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
